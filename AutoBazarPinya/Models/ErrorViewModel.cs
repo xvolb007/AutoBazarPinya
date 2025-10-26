5050c3e6fa856b4aa550f7fcd7de8f9e5ce98be9
Bohdan.Volkov <Bohdan.Volkov@giacom.com>
2025-10-26 18:15:03 +0100

    Add project files.

---
+++ AutoBazarPinya/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoBazarPinya.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
